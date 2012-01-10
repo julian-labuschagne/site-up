c5a207c5a59664ec78126f7fd5f1b205dfb561ec
Julian <julian@jlweb.co.za>
2012-01-10 14:43:35 +0200

    Added the Beep Command

--- site-up.sh
+++ site-up.sh
@@ -10,14 +10,15 @@ function check_status() {
 		STATUS_CODE=`curl -sL -m 5 -w "%{http_code}" "$line" -o /dev/null`
 		
 		if [ $STATUS_CODE -eq "200" ]; then
-		site_status="[OK]"
-		printf "%-66s \e[1;32m %10s \e[0m \n" $line $site_status
+			site_status="[OK]"
+			printf "%-66s \e[1;32m %10s \e[0m \n" $line $site_status
 		elif [ $STATUS_CODE -eq "403" ]; then
-		site_status="[OK]"
-		printf "%-66s \e[1;33m %10s \e[0m \n" $line $site_status
+			site_status="[OK]"
+			printf "%-66s \e[1;33m %10s \e[0m \n" $line $site_status
 		else
-		site_status="[NA]"
-		printf "%-66s \e[1;31m %10s \e[0m \n" $line $site_status
+			site_status="[NA]"
+			printf "%-66s \e[1;31m %10s \e[0m \n" $line $site_status
+			beep -e /dev/input/by-path/platform-pcspkr-event-spkr -f 800.7 -r 10 -d 200 -l 400
 		fi
 
 	done < urls.txt
@@ -28,7 +29,7 @@ function check_status() {
 	echo
 	echo "Done ... press Ctrl + c to exit"
 	 
-	sleep 5m
+	sleep 10m
 }
 
 while true;
